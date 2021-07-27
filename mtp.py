148c95653cc229e66ad87f0053be55f6bc655891
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-07-27 16:03:28 -0300

    debbuging

--- mtp.py
+++ mtp.py
@@ -2,6 +2,8 @@ import pdfkit
 import markdown
 import sys
 
-output_filename = sys.argv[1]
-markdown.markdownFromFile(input='markdown.md', output='index.html', extensions=['extra'])
-pdfkit.from_file('index.html', output_filename)
+mark_filename = sys.argv[1]
+pdf_filename = sys.argv[2]
+mark_text = open(mark_filename, 'r').read()
+html = markdown.markdown(mark_text, extensions=['extra'])
+pdfkit.from_string(html, pdf_filename)
